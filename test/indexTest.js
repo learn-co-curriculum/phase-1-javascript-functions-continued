fa366a9e3a39195e9fe5ebb66907ac4503d3023c
Steven G. Harms <steven.harms@flatironschool.com>
2019-05-07 15:11:27 -0400

    Clarify actionApplyer

--- test/indexTest.js
+++ test/indexTest.js
@@ -101,13 +101,14 @@ describe("index.js", () => {
         expect(actionApplyer(0, [])).to.equal(0)
       })
 
-      it("returns the correctly changed value", function() {
+      it("return 4, the result of the first arg, 13 being acted upon by several transformation functions", function() {
+        let message = "13, multiplied by 2, added to 1000 and then modulo 7 is 4. Apply each function in the Array of functions on the given base (13) OR on the result of the use of the previous function to get this result"
         arrayOfTransforms = [
           function(a){ return a * 2 },
           function(a){ return a + 1000},
           function(a){ return a % 7 }
         ]
-        expect(actionApplyer(13, arrayOfTransforms)).to.equal(4)
+        expect(actionApplyer(13, arrayOfTransforms)).to.equal(4, message)
       })
     })
 
