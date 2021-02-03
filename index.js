98b443dd7d9218085e3c4fb4a88191c192b6f19f
Liz Burton <liz.burton147@gmail.com>
2021-02-03 11:39:42 -0500

    Update solution code

--- index.js
+++ index.js
@@ -11,28 +11,3 @@ let wrapAdjective = function(style="*") {
     return `You are ${style}${adjective}${style}!`
   }
 }
-
-const Calculator = {
-  add: function(a,b) {
-    return a + b;
-  },
-  subtract: function(a,b) {
-    return a - b
-  },
-  multiply: function(a,b) {
-    return a * b
-  },
-  divide: function(a,b) {
-    return a / b;
-  }
-}
-
-let actionApplyer = function(start, ray) {
-  let a = start
-
-  for (let i = 0; i < ray.length; i++ ){
-    a = ray[i](a)
-  }
-
-  return a
-}
