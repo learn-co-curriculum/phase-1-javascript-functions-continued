d8f224cbdcfb02dd8788c3b16207785f90f89353
Liz Burton <liz.burton147@gmail.com>
2021-06-10 12:24:29 -0400

    AUTO: remove file-system from test files

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,6 @@
 const chai = require('chai')
 global.expect = chai.expect
-const fs = require('file-system')
+const fs = require('fs')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
 const babel = require('babel-core');
