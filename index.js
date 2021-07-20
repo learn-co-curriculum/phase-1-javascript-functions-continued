b24d61a4b936386b3ba6eed2733b1a6fa918c942
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-07-20 09:36:18 -0600

    commit tree 74e085a035cf0890d0fedc5905fe00d483c5da20

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return`This Saturday, I want to ${activity}!`
+  }
+  
+  mondayWork = function(work="go to the office") {
+    return `This Monday, I will ${work}.`
+  }
+  
+  function wrapAdjective(format="*") {
+    return function(adjective="special") {
+      return `You are ${format}${adjective}${format}!`
+    }
+  }
