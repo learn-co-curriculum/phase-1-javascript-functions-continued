c6e931b9b5d014fe7ecdda8c73f40dc3da7368d8
Eric Einerson <eaeiner.gmail.co>
2022-01-12 11:43:58 -0700

    commit tree 119fb9e3d91d21416d5f23827fd4217f9fca2fdd

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity){
+        if (activity == undefined) {
+            return `This Saturday, I want to roller-skate!` 
+        }
+        else {
+        return `This Saturday, I want to ${activity}!`
+}
+}
+
+const mondayWork = function(activity2){
+    if (activity2 == undefined) {
+        return "This Monday, I will go to the office.";
+    }
+    else {
+        return `This Monday, I will ${activity2}.`;
+    }
+}
+
+function wrapAdjective(Flair='*'){
+    return function inner(adjective="special"){
+        return (`You are ${Flair}${adjective}${Flair}!`)
+    }
+}
