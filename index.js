f59f1ea2bca6966a0663a477427a71c5f91fd6bf
Elijah Mbugi <mukundielijah@gmail.com>
2022-04-07 20:44:10 +0300

    commit tree cf560e11aa9cb122cf740f34bca98b0ea3c91e30

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+saturdayFun();
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(adj='special') {
+    let result = '*'
+    return function () {
+    let emphatic = "a hard worker"
+    return (`You are ${result}${emphatic}${result}!`);
+    };
+};
+
+function wrapAdjective(adj='special') {
+    let result = '||'
+    return function () {
+    let emphatic = "a dedicated programmer"
+    return (`You are ${result}${emphatic}${result}!`);
+    };
+};
+
