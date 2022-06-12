90d5302d3a4c1feaad421c7d2b973c18c10a0119
jemel <jemel.defreitas@gmail.com>
2022-06-12 13:04:04 -0500

    commit tree 30784175951b60a0641edcdde21b124de0e4c1cd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity= 'roller-skate') {
+    return 'This Saturday, I want to ' + activity + '!'
+}// code your solution here
+saturdayFun();
+
+function mondayWork(activity= 'go to the office') {
+    return 'This Monday, I will ' + activity + '.'
+}
+mondayWork
+();
+
+function wrapAdjective(flair = "*"){
+     return function (parameter = "special"){
+        return 'You are ' + flair + parameter + flair + '!';
+    };
+    //return innerFunction();
+}
