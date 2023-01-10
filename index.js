f0408b347dfb497c13b2297272d730066702c623
Camron <cawe219@gmail.com>
2023-01-09 22:42:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(value){
+    if (value == undefined){
+        return "This Saturday, I want to roller-skate!";
+    } else {
+        return `This Saturday, I want to ${value}!`
+    }
+}
+
+
+function mondayWork(value){
+    if (value == undefined){
+        return "This Monday, I will go to the office."
+    } else {
+        return `This Monday, I will ${value}.`
+    }
+}
+
+function wrapAdjective(value){
+    return function (emphatic){
+        return `You are ${value}${emphatic}${value}!`
+    }
+}
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
