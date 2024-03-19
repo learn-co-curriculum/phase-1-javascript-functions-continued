9790a8a4efa8aa80d737401b5c41a245ffa62400
Brian <brian.m.muigai@gmail.com>
2024-03-19 21:23:14 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(opt="roller-skate"){
+  return `This Saturday, I want to ${opt}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(opt="go to the office"){
+    return `This Monday, I will ${opt}.`;
+}
+
+function wrapAdjective(adjective="*"){
+    const innerFunction= function (parameter="special"){
+        return `You are ${adjective}${parameter}${adjective}!`;
+    };
+    return innerFunction
+}
